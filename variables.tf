027be85c8c843d3fd8b9163501c3026b290637c9
Aashutosh Singh <84022041+aashutosh-tech@users.noreply.github.com>
2023-07-24 20:58:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "access"{
-    default ="ASIAZY75DFSOLMEDCUH5"
+    default ="ASIAZY75DFSOCFGVL6LU"
 }
 variable "secret"{
-    default ="dzku6ISfdUATNKyDUtQCt5I7AawqqdSPHMR2SYmk"
+    default ="xCU5sxK3wIcjQ6TozUoBgk/BkxxXI2qLdbuqdkwM"
 }
 variable "token"{
-    default = "FwoGZXIvYXdzEIL//////////wEaDHyxdR8vnRGvshua5SK6Ae8BrKn2cimNRF5dm2fVGR54hXBlnLanl0vBaY5+OJ38Tg9Wc24tTPTfCycC4e4VEWt08D8ew02901Gynu7FLcG872feA5VNIXYQMDrf6VyhweiMpKheHQk2zJD/qRna8/eiOHOPg1/LKWM4f99cagfxJ5+QHzQexpoyiPSqH86ZDumDDlKOGszFzNquXmaENay4IVyYmKkUI8RsDxiIEJE26lmM2IcBZcTYcrqDD8KG8yWbaQphg/mjRSicmvelBjItph4oKvh3dBNYm3N0ZnsmtWfKpvBVvXlbUlrpt9vUW3A8fmSra/DpHKmuOVr1"
+    default = "FwoGZXIvYXdzEJH//////////wEaDBWFLsjBLH7IHAEP0iK6AWtHb+GCvUFBX1UzFiIsf9ebLNt9Gj0lpkS4QSffb7KO+9/ov+0v6krfXF+NL3wrvWpjC4LQsLm+5n3mkWQUeMCcL56Sg/3L92l8BTZxQb+VC1mDkHTY/9kLJpF3LCocA2QHW5ypNZ6jSsfvrPc59XArd8mNkKkytKAvlLviaaosOdc20V1R51+dEr98Y03go/4D2bbYIEPB63jfng9n9oSfJf22Iq4nHmLoEgbxDysXo8jkl6QLwYbA+yifsPqlBjItlgavK9pIr1/5zJhMgtU27Ioo87MTPieH9z7TpN/kM9zMogWbWx6ZY/wtS4M1"
 }
